cf6019eed58c199ca5b33389a4d0b52d76344f2e
kalrashivam <shivamkalra2017@gmail.com>
2021-07-06 22:00:00 +0530

    change

--- contracts/PoolFactory.sol
+++ contracts/PoolFactory.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.3;
 
 import { FriendPool } from "./FriendPool.sol";
 
-// contract solidity {
-//     function createPool(uint _totalMembers, uint _targetAmount) public returns (uint256) {
-//         require(poolMember[msg.sender].exists, "Existing pool memeber cannot be an owner");
-//         poolId++;
-//         Pools[poolId] = Pool(_totalMembers, _targetAmount);
-//         poolOwner[poolId] = msg.sender;
-//         poolMember[msg.sender] = data(true, poolId);
+contract solidity {
+    function createPool(uint _totalMembers, uint _targetAmount) public returns (uint256) {
+        require(poolMember[msg.sender].exists, "Existing pool memeber cannot be an owner");
+        poolId++;
+        Pools[poolId] = Pool(_totalMembers, _targetAmount);
+        poolOwner[poolId] = msg.sender;
+        poolMember[msg.sender] = data(true, poolId);
 
-//         return poolId;
-//     }
-// }
+        return poolId;
+    }
+}
